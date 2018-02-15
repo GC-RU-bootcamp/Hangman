70f9dbd9717313d971df76a7b737fa2f4fdab19e
Cender <CenderG@win.dowjones.net>
2018-02-15 00:15:32 -0500

    my message

--- data.js
+++ data.js
@@ -1,77 +1,208 @@
 var bands = [
-	{
-	Name: "Led Zepplin",
-	Origin: "England",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} , // band 0
-             {
-	Name: "The Beatles",
-	Origin: "England",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} , // band 1
-{
-	Name: "Areosmith",
-	Origin: "England",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} , // band 3
-{
-	Name: "Aerosmith",
-	Origin: "United States",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} , // band 3
-{
-	Name: "Pink Floyd",
-	Origin: "England",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} , // band 4
-{
-	Name: "The Who",
-	Origin: "England",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} , // band 5
-{
-	Name: "Queen",
-	Origin: "England",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} , // band 6   Lynyrd Skynyrd
-{
-	Name: "Lynyrd Skynyrd",
-	Origin: "United States",
-	Dates: "",
-	Hint:"",
-	Info: "",
-	Albums: [],
-	Songs: []
-} // band 6   
-
-
-
+  {
+    Name: "Led Zepplin",
+    origin: "London, England",
+    dates: "1968 - 1980",
+    info: "New Yardbirds",
+    leadSinger: "Robert Plant",
+    bandMembers: "Jimmy Page,	Robert Plant,	John Paul Jones,	John Bonham",
+    albums: ["Immigrant Song", "Gallows Pole", "Kashmir", "Stairway to Heaven"],
+    songs: ["Immigrant Song", "Gallows Pole", "Kashmir", "Stairway to Heaven"]
+  }, // band 0
+  {
+    Name: "The Beatles",
+    origin: "Liverpool, England",
+    dates: "1957 - 1970",
+    info: "The fab four",
+    leadSinger: "each member sung a song on each album",
+    bandMembers: "John Lennon, 	Paul McCartney, 	George Harrison, 	Ringo Starr",
+    albums: [
+      "Rubber Soul ",
+      "Yellow Submarine",
+      "A Hard Day's Night",
+      "The White Album"
+    ],
+    songs: [
+      "Let it be ",
+      "Hey Jude ",
+      "Yesterday ",
+      "A day in a life ",
+      "Here comes the sun ",
+      "Come together ",
+      "Penny Lane ",
+      "Something ",
+      "Eleanor Rigby ",
+      "Twist and Shout "
+    ]
+  }, // band 1
+  {
+    Name: "Aerosmith",
+    origin: "Boston Mass, U.S.",
+    dates: "Since 1970",
+    dates: "1970 - present",
+    info: "The Bad Boys from Boston",
+    leadSinger: "Steven Tyler",
+    bandMembers:
+      "Steven Tyler, Joe Perry, Brad Whitford, Tom Hamilton, Joey Kramer",
+    albums: [
+      "Get Your Wings (1974)",
+      "Toys in the Attic (1975)",
+      "Rocks (1976)",
+      "Draw the Line (1977)"
+    ],
+    songs: [
+      "Come Together",
+      "Dude (Looks Like a Lady)",
+      "Dream On",
+      "Sweet Emotion",
+      "Dream On",
+      "Walk This Way"
+    ]
+  }, // band 2
+  {
+    Name: "Pink Floyd",
+    origin: "London, England",
+    dates: "1965–1995 2005 2012–2014",
+    info: "Psychedelic rock",
+    leadSinger: "Roger Waters",
+    bandMembers:
+      "Nick Mason, Roger Waters, Richard Wright, Syd Barrett,	David Gilmour",
+    albums: [
+      "Ummagumma",
+      "Animals",
+      "Wish You Were Here",
+      "The Wall",
+      "The Dark Side of the Moon"
+    ],
+    songs: [
+      "Brain Damage",
+      "Comfortably Numb",
+      "Have a Cigar",
+      "Hey You",
+      "Young Lust",
+      "Another Brick in the Wall "
+    ]
+  }, // band 3
+  {
+    Name: "The Who",
+    origin: "London, England",
+    dates: "1964 - present",
+    info: "The Detours & 	The High Numbers",
+    leadSinger: "Roger Daltry",
+    bandMembers:
+      "Pete Townshend,,John Entwistle,	Doug Sandom,	Keith Moon,	Kenney Jones, Roger Daltry",
+    albums: [
+      "My Generation (1965)",
+      "Tommy (1969)",
+      "Whos Next (1971)",
+      "Quadrophenia (1973)",
+      "The Who by Numbers (1975)",
+      "Who Are You (1978)"
+    ],
+    songs: [
+      "Pinball Wizard",
+      "5.15",
+      "Who Are You",
+      "Love, Reign Over Me",
+      "I Can See for Miles",
+      "Won&quot;t Get Fooled Again",
+      "My Generation",
+      "Baba O&quot;Riley"
+    ]
+  }, // band 4
+  {
+    Name: "Queen",
+    origin: "London, England",
+    dates: "1970 - present",
+    info: "Anthem Rock",
+    leadSinger: "Freddie Mercury",
+    bandMembers: "Freddie Mercury, John Deacon, Brian May,	Roger Taylor",
+    albums: [
+      "Sheer Heart Attack (1974), ",
+      "A Night at the Opera (1975), ",
+      "A Day at the Races (1976), ",
+      "News of the World (1977), ",
+      "Jazz (1978)"
+    ],
+    songs: [
+      "We Are the Champions",
+      "Tie Your Mother Down",
+      "Fat Bottomed Girls",
+      "Under Pressure",
+      "Bohemian Rhapsody"
+    ]
+  }, // band 5   Lynyrd Skynyrd
+  {
+    Name: "Lynyrd Skynyrd",
+    origin: "Jacksonville, Florida, U.S.",
+    dates: "1964–1977 1979 1987–present",
+    info: "Southern rock",
+    leadSinger: "Ronnie Van Zant",
+    bandMembers: "Ronnie Van Zant, Allen Collins, Bob Burns Larry Junstrom",
+    albums: [
+      "Second Helping",
+      "Nuthin' Fancy",
+      "Gimme Back My Bullets",
+      "Street Survivors"
+    ],
+    songs: [
+      "Gimme Three Steps",
+      "That Smell",
+      "What's Your Name",
+      "Sweet Home Alabama",
+      "Free Bird"
+    ]
+  },
+  // band 6   the Eagles
+  {
+    Name: "Eagles",
+    origin: "Los Angeles, California, U.S.",
+    dates: "1971–1980 1994–2016",
+    info: "	Rock country, rock soft, rock folk, rock",
+    leadSinger: "Don Henley, Don Felder",
+    bandMembers:
+      "Don Henley, Glenn Frey, Bernie Leadon, 	Randy Meisner, 	Don Felder, Joe Walsh,	Timothy B. Schmit",
+    albums: [
+      "Desperado (1973) ",
+      "On the Border (1974) ",
+      "One of These Nights (1975) ",
+      "Hotel California (1976) ",
+      "The Long Run (1979) "
+    ],
+    songs: [
+      "Life in the Fast Lane",
+      "Lyin Eyes",
+      "Hotel California",
+      "One Of These Nights",
+      "Peaceful Easy Feeling",
+      "Take It Easy",
+      "Tequila Sunrise",
+      "Witchy Woman"
+    ]
+  }, // band 7   Deep Purple
+  {
+    Name: "Deep Purple",
+    origin: "	Hertford, Hertfordshire, England",
+    dates: "1968 - present",
+    info: "The globe's loudest band",
+    leadSinger: "Ian Gillan",
+    bandMembers:
+      "Jon Lord, Ritchie Blackmore Ian Paice, Roger Glover, Ian Gillan ",
+    albums: [
+      "Fireball (1971)",
+      "Machine Head (1972)",
+      "Who Do We Think We Are (1973)",
+      "Burn (1974)",
+      "Stormbringer (1974)"
+    ],
+    songs: [
+      "Hush",
+      "Black Night",
+      "Burn",
+      "Woman From Tokyo",
+      "Highway Star",
+      "Strange Kind Of Woman",
+      "Smoke on the Water"
+    ]
+  }
 ];
--- javascript.js
+++ javascript.js
@@ -1,14 +1,15 @@
+// Stats values
 var countHTML = document.getElementById("count");
 var guessesHTML = document.getElementById("letter");
 var winsHTML = document.getElementById("wins");
 var lossesHTML = document.getElementById("losses");
 var nameHTML = document.getElementById("name");
 var name2HTML = document.getElementById("name2");
-var name3HTML = document.getElementById("name3");
 var winsHTML = document.getElementById("wins");
 var lossesHTML = document.getElementById("losses");
 var timeHTML = document.getElementById("time");
-
+var newGameBtnHTML = document.getElementById("new-game-btn");
+// Stats containers
 var countRowHTML = document.getElementById("count-row");
 var guessesRowHTML = document.getElementById("letter-row");
 var winsRowHTML = document.getElementById("wins-row");
@@ -18,6 +19,20 @@ var name2RowHTML = document.getElementById("name2-row");
 var winsRowHTML = document.getElementById("wins-row");
 var lossesRowHTML = document.getElementById("losses-row");
 var timeRowHTML = document.getElementById("time-row");
+// Modal ids
+var modalTitleHTML = document.getElementById("modal-title");
+var modalHeaderHTML = document.getElementById("modal-header");
+var modalBodyHTML = document.getElementById("modal-body");
+var modalBtnHTML = document.getElementById("modal-btn");
+var modalBtnXHTML = document.getElementById("modal-btn-x");
+// Hints values
+var hintActiveYearsHTML = document.getElementById("active-years");
+var hintoriginHTML = document.getElementById("origin");
+var hintinfoHTML = document.getElementById("info");
+var hintLeadSingerHTML = document.getElementById("lead-singer");
+var hintBandMembersHTML = document.getElementById("band-members");
+var hintAlbumsHTML = document.getElementById("albums");
+var hintSongsHTML = document.getElementById("songs");
 
 // Create reference to our reset button
 // var newGameButton = document.getElementById("new-game-btn");
@@ -32,7 +47,72 @@ function pauseAudio() {
   x.pause();
 }
 
+var hints = {
+  ay: "--",
+  orig: "--",
+  info: "--",
+  singer: "--",
+  bandmem: "--",
+  albums: "--",
+  songs: "--",
+  ayT: 1,
+  originT: 3,
+  infoT: 4,
+  singerT: 7,
+  bandmemT: 8,
+  albumsT: 9,
+  songsT: 11,
+  initHints: function() {
+    this.ay = "--";
+    this.origin = "--";
+    this.info = "--";
+    this.singer = "--";
+    this.bandmem = "--";
+    this.albums = "--";
+    this.songs = "--";
+  },
+  displayHints: function() {
+    hintActiveYearsHTML.innerHTML = this.ay;
+    hintoriginHTML.innerHTML = this.origin;
+    hintinfoHTML.innerHTML = this.info;
+    hintLeadSingerHTML.innerHTML = this.singer;
+    hintBandMembersHTML.innerHTML = this.bandmem;
+    hintAlbumsHTML.innerHTML = this.albums;
+    hintSongsHTML.innerHTML = this.songs;
+  },
+  updateHints: function(time, maxTime) {
+    var T = maxTime - time;
+
+    switch (T) {
+      case this.ayT:
+        this.ay = bands[game.bandOffset].dates;
+        break;
+      case this.originT:
+        this.origin = bands[game.bandOffset].origin;
+        break;
+      case this.infoT:
+        this.info = bands[game.bandOffset].info;
+        break;
+      case this.singerT:
+        this.singer = bands[game.bandOffset].leadSinger;
+        break;
+      case this.bandmemT:
+        this.bandmem = bands[game.bandOffset].bandMembers;
+        break;
+      case this.albumsT:
+        this.albums = bands[game.bandOffset].albums;
+        break;
+      case this.songsT:
+        this.songs = bands[game.bandOffset].songs;
+        break;
+      default:
+    } // switch
+  } // updateHints
+}; //hints object
+
 var game = {
+  // START of Varables
+  // START of Varables
   band: "", // Name to guess
   bandMask: "", // Name to guess
   bandOffset: 0, //offset to band to guess
@@ -45,8 +125,35 @@ var game = {
   guessMax: 12, //game constant # of guess allowed
 
   cntDown: 0, // count Down timer in seconds
- // cntDownInit: 45, //game constant # of seconds allowed
-  cntDownInit: 10, //game constant # of seconds allowed
+  firstGame: true, // is this the first game ?
+  dialogOpen: false,
+  // cntDownInit: 45, //game constant # of seconds allowed
+  cntDownInit: 30, //game constant # of seconds allowed
+  timeout: false,
+  // END of Varables -- start member functions() AKA methods
+  // END of Varables -- start member functions() AKA methods
+  newGame: function() {
+    // start from scratch (including win/loses)
+    if (game.firstGame === true) {
+      game.initWinCnt();
+      game.firstGame = false;
+    }
+    game.dialogOpen = false;
+    //init game
+    game.initGame();
+    game.initTimer();
+    game.updateDisplay();
+    hints.initHints();
+    hints.displayHints();
+
+    if (game.timeout !== false) {
+      clearTimeout(game.timeout);
+    }
+
+    game.timeout = setTimeout(game.cntDownFunc, 1000);
+    // init hints
+    // TBD
+  },
   initWinCnt: function() {
     this.winsCnt = 0;
     //winsHTML.textContent = this.winsCnt.toString();
@@ -66,6 +173,7 @@ var game = {
     // countHTML.textContent = this.guessCnt.toString();
     // randomly pick band
     this.bandOffset = Math.floor(Math.random() * bands.length);
+    //this.bandOffset++; // debugging data
     this.band = bands[this.bandOffset].Name;
     this.bandMask = this.createMask(this.band);
     // nameHTML.textContent = this.bandMask;
@@ -82,12 +190,14 @@ var game = {
 
     nameHTML.textContent = this.bandMask;
     name2HTML.textContent = this.band;
-    name3HTML.textContent = this.band;
     winsHTML.textContent = this.winsCnt.toString();
 
     lossesHTML.textContent = this.lossCnt.toString();
     timeHTML.textContent = this.cntDown.toString();
     return 0;
+  },
+  updateModalDisplay: function(bannermgs, answer) {
+    modalBodyHTML.textContent = answer;
   },
   createMask: function(str) {
     var s = "";
@@ -130,11 +240,19 @@ var game = {
         // Guess all the letters? // Is this a winner?
         if (this.isWinner(this.bandMask)) {
           this.winsCnt++;
-          this.initGame();
+          game.updateDisplay();
+          game.showDialog("You are a Winner", this.band);
+          // game.initTimer();
+          // game.initGame();
+          // this.initGame();
         } else if (this.guessCnt === 0) {
           // Ran out of guesses?
           this.lossCnt++;
-          this.initGame();
+          game.updateDisplay();
+          game.showDialog("Sorry, you lost", this.band);
+          // game.initTimer();
+          // game.initGame();
+          // this.initGame();
         }
       }
       this.updateDisplay();
@@ -168,25 +286,37 @@ var game = {
     }
     return retval;
   },
-  showDialog: function() {
-    $("#myModal").modal();
+  showDialog: function(headMsg, bodyMsg) {
+    modalHeaderHTML.textContent = headMsg;
+    modalBodyHTML.textContent = bodyMsg;
+
+    $("#myModal").modal({
+      backdrop: "static",
+      keyboard: false // to prevent closing with Esc button (if you want this too)
+    });
+
+    this.dialogOpen = true;
     // $("#myModal").on("shown.bs.modal", function() {
-      // $("#myInput").focus();
+    // $("#myInput").focus();
     // });
   },
   cntDownFunc: function() {
-    game.cntDown--;
+    // stop counter when dialog is Open
+    if (game.dialogOpen !== true) {
+      game.cntDown--;
+    }
 
-    if (game.cntDown === 0) {
+    if (game.cntDown === 0 && game.dialogOpen !== true) {
       game.lossCnt++;
-      game.showDialog();
-      game.initTimer();
-      game.initGame();
+      game.showDialog("Sorry, you lost", game.band);
+      // game.initTimer();
+      // game.initGame();
       game.updateDisplay();
-    } 
-      timeHTML.textContent = game.cntDown.toString();
-      setTimeout(game.cntDownFunc, 1000);
-    
+    }
+    hints.updateHints(game.cntDown, game.cntDownInit);
+    hints.displayHints();
+    timeHTML.textContent = game.cntDown.toString();
+    game.timeout = setTimeout(game.cntDownFunc, 1000);
   }
   // ,
   // WORKING HERE
@@ -209,16 +339,21 @@ var game = {
   // }
 };
 
-game.initWinCnt();
-game.initGame();
-game.initTimer();
-game.updateDisplay();
+// game.initWinCnt();
+// game.initGame();
+// game.initTimer();
+// game.updateDisplay();
+
+//game.newGame();
 
 // setup countdown timer 1 second
-setTimeout(game.cntDownFunc, 1000);
 
 // Add 'click' event listener
-// newGameButton.addEventListener("click", game.newGame);
+modalBtnHTML.addEventListener("click", game.newGame);
+
+modalBtnXHTML.addEventListener("click", game.newGame);
+
+newGameBtnHTML.addEventListener("click", game.newGame);
 
 //  Listen for keyup event on document (DOM)
 document.onkeyup = function(event) {
